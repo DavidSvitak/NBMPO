a88851599926fa3670fed3850922b0dcf536eba5
Sviták David Bc. <svitakda@gmail.com>
2025-06-14 22:07:52 +0200

    whole magnetic phase diagrmas

--- cp/whole_diagram.py
+++ cp/whole_diagram.py
@@ -4,11 +4,12 @@ import pandas as pd
 import re
 from matplotlib.lines import Line2D
 
-
-# Specific heat
+# Along c axis 
 fig, ax = plt.subplots()
 pattern = re.compile(r'([^,]*),([^,]*),([^,]*)')
-with open("cp/extremes_along_ab_specific_heat.txt", "r") as txt:
+with open("cp/extremes_along_c_specific_heat.txt", "r") as txt:
+    x_data_cp = []
+    y_data_cp = []
     for line in str.split(txt.read(), "\n"):
         line = line.strip()
         match = pattern.match(line)
@@ -17,12 +18,15 @@ with open("cp/extremes_along_ab_specific_heat.txt", "r") as txt:
             if tt == "T(K)":
                 continue
             else:
-                tt = float(tt)
-                bb = float(bb)
-                plt.plot(tt, bb, "ro")
+                x_data_cp.append(float(tt))
+                y_data_cp.append(float(bb))
+    
+plt.plot(x_data_cp, y_data_cp, "ro", label = r"C$_p$")
 
 
-with open("cp/H_along_ab_transitions", "r") as txt:
+with open("cp/H_along_c_transitions.txt", "r") as txt:
+    x_data_n = []
+    y_data_n = []
     for line in txt:
         line = line.strip()
         tt, hh = line.split(",")
@@ -30,40 +34,65 @@ with open("cp/H_along_ab_transitions", "r") as txt:
             continue
         tt = float(tt)
         hh = float(hh)
-        plt.plot(tt, hh, "bo")
+        x_data_n.append(tt)
+        y_data_n.append(hh)
+plt.plot(x_data_n, y_data_n, "bo", label = "Neutrons")
 
-"""
-pattern = re.compile(r"([^,]*),([^,]*),([^,]*),([^,]*),([^,]*),([^,]*)")
-with open("cp/extremes_along_ab_neutrons.txt") as txt:
+plt.legend()
+plt.xlabel("T(K)")
+plt.ylabel("H(T)")
+plt.ylim(0, 6)
+plt.text(0.6, 0.3, "AFM1")
+plt.text(0.4, 1.1, "phase A")
+plt.text(1.15, 0.3, "AFM2")
+plt.text(0.8, 2, "UUD phase")
+plt.title(r"Magnetic phase diagram with magnetic field applied along $c$ axis")
+plt.savefig("cp/whole_diagram_c.png")
+
+fig, ax = plt.subplots()
+pattern = re.compile(r'([^,]*),([^,]*),([^,]*)')
+with open("cp/extremes_along_ab_specific_heat.txt", "r") as txt:
+    x_data_cp = []
+    y_data_cp = []
     for line in str.split(txt.read(), "\n"):
         line = line.strip()
         match = pattern.match(line)
         if match:
-            tt, h1, h2, h3, h4, h5 = match.groups()
-            if tt == "T":
+            tt, bb, hc = match.groups()
+            if tt == "T(K)":
                 continue
             else:
-                for h in [h1, h2, h3, h4, h5]:
-                    if h :
-                        tt = float(tt)
-                        h = float(h)
-                        plt.plot(tt, h, "bo")
-
-ankit_data = pd.read_csv("cp/sketch_H_par_ab.dat", header=1, names=["H", "T1", "T2"], sep=",")
-for i in range(len(ankit_data)):
-    plt.plot(ankit_data["T1"][i], ankit_data["H"][i], "ko")
-    if ankit_data["T1"][i]:
-        plt.plot(ankit_data["T2"][i], ankit_data["H"][i], "ko")
+                x_data_cp.append(float(tt)-0.04)
+                y_data_cp.append(float(bb))
+    
+plt.plot(x_data_cp, y_data_cp, "ro", label = r"C$_p$")
 
 
+with open("cp/H_along_ab_transitions.txt", "r") as txt:
+    x_data_n = []
+    y_data_n = []
+    for line in txt:
+        line = line.strip()
+        tt, hh = line.split(",")
+        if tt == "T(K)" and hh == "H(T)":
+            continue
+        tt = float(tt)
+        hh = float(hh)
+        x_data_n.append(tt)
+        y_data_n.append(hh)
+plt.plot(x_data_n, y_data_n, "bo", label = "Neutrons")
 
-legend_elements = [
-    Line2D([0], [0], marker="o", color="w", label="David data treatment", markerfacecolor='r', markersize=7),
-    Line2D([0], [0], marker="o", color="w", label="Neutrons", markerfacecolor='b', markersize=7),
-    Line2D([0], [0], marker="o", color="w", label="Ankit data treatment", markerfacecolor='k', markersize=7)
-    ]
-"""
+plt.legend()
+plt.xlabel("T(K)")
+plt.ylabel("H(T)")
+plt.ylim(0, 6)
+plt.text(0.6, 0.3, "AFM1")
+plt.text(0.4, 1.4, "phase C")
+plt.text(1.05, 2.5, "AFM2")
+plt.text(0.4, 2, "phase D")
+plt.text(0.4, 3.2, "phase E")
+plt.title(r"Magnetic phase diagram with magnetic field applied in the $ab$ plane")
+plt.savefig("cp/whole_diagram_ab.png")
+plt.show()
 
-#ax.legend(handles=legend_elements)
-#plt.savefig("cp/Data_treatment_comparison_ab.jpg")
 plt.show()
